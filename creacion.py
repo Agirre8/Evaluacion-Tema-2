a5b75358801cef5cf3391836ddb9f4e240d174af
Agirre8 <inagi30@gmail.com>
2022-10-07 15:51:05 +0200

    funcion calificacion

--- creacion.py
+++ creacion.py
@@ -6,3 +6,8 @@ class Alumno:
         self.nombre = nombre
         self.nota = nota
     
+    def calificacion (nombre, nota):
+        if nota<5:
+            calificacion(nombre)
+        else:
+            return print("El alumno:", nombre, "ha aprobado")
