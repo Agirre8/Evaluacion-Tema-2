10c64ae0e291b6bc99827eeaf8a2fa0914e97a86
Agirre8 <inagi30@gmail.com>
2022-10-07 15:57:00 +0200

    funcion str ejercicio 2

---
+++ Ejercicio2.py
@@ -0,0 +1,14 @@
+class Alumno:
+
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+    
+    def calificacion (nombre, nota):
+        if nota<5:
+            print("El alumno:", nombre, "ha suspendido")
+        else:
+            return print("El alumno:", nombre, "ha aprobado")
+    def __str__(self): 
+        return "Lo que quiero mostrar"
+    
