7a3beea39eaf238d164c38b7bdbfce512d28ea5e
Agirre8 <inagi30@gmail.com>
2022-10-07 16:52:58 +0200

    excepcion lista

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,4 +1,21 @@
 def dividir (numerador, divisor):
     return numerador/divisor
-resultado = dividir(15, 5)
-print(resultado)
+
+try:
+    resultado = dividir(7, 0)
+    print(resultado)
+except ZeroDivisionError:
+    print("No se puede dividir entre 0")
+
+
+lista = [4,7,30,23,5]
+
+index=10
+
+if 0 <= index < len(lista):
+    lista[index]
+else:
+    print("Elemento fuera de la lista")
+
+
+
